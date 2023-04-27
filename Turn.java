2d11987ab013d4f5bcf90b752b21fa2a00cc4f45
hanekaiser <99454872+hanekaiser@users.noreply.github.com>
2023-04-27 19:58:40 -0400

    Update Turn.java

--- Turn.java
+++ Turn.java
@@ -21,8 +21,10 @@ public class Turn {
         //Complex point scoring
         threeKind(dice);
         fourKind(dice);
+        fullHouse(dice);
         straightSmall(dice);
         straightLarge(dice);
+        chance(dice);
         yahtzee(dice);
         rollNum--;
     }
@@ -64,22 +66,102 @@ public class Turn {
 
 
     public int threeKind(Dice[] dice) {
+        int sum = 0;
+        int[] count = new int[7];
+        for(int i = 0; i < dice.length; i++){
+            count[dice[i].getNum()]++;
+            sum += dice[i].getNum();
+        }
+        for(int i = 1; i <= 6; i++){
+            if(count[i] >= 3){
+                return sum;
+            }
+        }
         return 0;
     }
 
     public int fourKind(Dice[] dice) {
+        int sum = 0;
+        int[] count = new int[7];
+        for(int i = 0; i < dice.length; i++){
+            count[dice[i].getNum()]++;
+            sum += dice[i].getNum();
+        }
+        for(int i = 1; i <= 6; i++){
+            if(count[i] >= 4){
+                return sum;
+            }
+        }
         return 0;
     }
 
     public int yahtzee(Dice[] dice) {
+        int[] count = new int[7];
+        for(int i = 0; i < dice.length; i++){
+            count[dice[i].getNum()]++;
+        }
+        
+        for(int i = 1; i <= 6; i++){
+            if(count[i] == 5){
+                return 50;
+            }
+        }
         return 0;
     }
 
+    public int fullHouse(Dice[] dice){
+        int[] count = new int[7];
+        for(int i = 0; i < dice.length; i++){
+            count[dice[i].getNum()]++;
+        }
+        int twos = 0;
+        int threes = 0;
+        for(int i = 1; i <= 6; i++){
+            if(count[i] == 2){
+                twos = i;
+            }
+            if(count[i] == 3){
+                threes = i;
+            }
+        }
+        if(twos > 0 && threes > 0){
+            return 25;
+        }
+        return 0;
+    }
+
+    public int chance(Dice[] dice){
+        int total = 0;
+        for(int i = 0; i < dice.length; i++){
+            total += dice[i].getNum();
+        }
+
+        return total;
+    }
+
     public int straightSmall(Dice[] dice) {
+        if(checkDiceSimple(dice, 1) == 1 && checkDiceSimple(dice, 2) == 2 && checkDiceSimple(dice, 3) == 3 
+        && checkDiceSimple(dice, 4) == 4) {
+            return 30;
+        } else if(checkDiceSimple(dice, 2) == 2 && checkDiceSimple(dice, 3) == 3 
+        && checkDiceSimple(dice, 4) == 4 && checkDiceSimple(dice, 5) == 5) {
+            return 30;
+        } else if(checkDiceSimple(dice, 6) == 6 && checkDiceSimple(dice, 3) == 3 
+        && checkDiceSimple(dice, 4) == 4 && checkDiceSimple(dice, 5) == 5) {
+            return 30;
+        }
         return 0;
     }
 
     public int straightLarge(Dice[] dice) {
+        if(checkDiceSimple(dice, 1) == 1 && checkDiceSimple(dice, 2) == 2 && checkDiceSimple(dice, 3) == 3 
+        && checkDiceSimple(dice, 4) == 4 && checkDiceSimple(dice, 5) == 5) {
+            return 40;
+        } else if(checkDiceSimple(dice, 6) == 6 && checkDiceSimple(dice, 2) == 2 && checkDiceSimple(dice, 3) == 3 
+        && checkDiceSimple(dice, 4) == 4 && checkDiceSimple(dice, 5) == 5) {
+            return 40;
+        }
+
         return 0;
     }
 }
